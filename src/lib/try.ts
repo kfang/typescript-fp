5b118ff4c2071561f011123cfb5f5eae751d4e97
Kevin Fang <kevin.fang@headspace.com>
2018-10-30 19:55:21 -0700

    implement more methods

--- src/lib/try.ts
+++ src/lib/try.ts
@@ -1,62 +1,81 @@
-import { Optional } from "./optional";
+import { Optional } from './optional';
 
 export abstract class Try<A> {
-
-    public static of<B>(fn: () => B): Try<B | Error> {
-        try {
-            const value = fn();
-            return new Success(value);
-        } catch (error) {
-            return new Failure(error);
-        }
+  public static of<B>(fn: () => B): Try<B> {
+    try {
+      const value = fn();
+      return new Success(value);
+    } catch (error) {
+      return new Failure(error);
     }
+  }
 
-    protected readonly value: A;
-
-    constructor(v: A) {
-        this.value = v;
+  public static pOf<B>(fn: () => Promise<B>): Promise<Try<B>> {
+    try {
+      return fn()
+        .then(v => new Success(v))
+        .catch(e => new Failure(e));
+    } catch (e) {
+      return Promise.resolve(new Failure(e));
     }
+  }
 
-    public abstract get(): A
+  protected readonly value: A;
 
-    public abstract isSuccess(): boolean
+  constructor(v: A) {
+    this.value = v;
+  }
 
-    public abstract isFailure(): boolean
+  public abstract get(): A;
 
-    public toOption(): Optional<A> {
-        return this.isSuccess() ? Optional.of(this.value) : Optional.empty();
-    }
+  public abstract getOrElse(defaultValue: A): A;
 
+  public abstract isSuccess(): boolean;
+
+  public abstract isFailure(): boolean;
+
+  public abstract toOption(): Optional<A>;
 }
 
-export class Failure extends Try<Error> {
+export class Failure<A> extends Try<A> {
+  public get(): A {
+    throw this.value;
+  }
+  public getOrElse(defaultValue: A): A {
+    return defaultValue;
+  }
 
-    public get(): Error {
-        throw this.value;
-    }
+  public isSuccess(): boolean {
+    return false;
+  }
 
-    public isSuccess(): boolean {
-        return false;
-    }
+  public isFailure(): boolean {
+    return true;
+  }
 
-    public isFailure(): boolean {
-        return true;
-    }
-
+  public toOption(): Optional<A> {
+    return Optional.of(this.value);
+  }
 }
 
 export class Success<A> extends Try<A> {
+  public get(): A {
+    return this.value;
+  }
 
-    public get(): A {
-        return this.value;
-    }
+  public getOrElse(): A {
+    return this.value;
+  }
 
-    public isSuccess(): boolean {
-        return true;
-    }
+  public isSuccess(): boolean {
+    return true;
+  }
 
-    public isFailure(): boolean {
-        return false;
-    }
+  public isFailure(): boolean {
+    return false;
+  }
 
+  public toOption(): Optional<A> {
+    return Optional.empty();
+  }
 }
