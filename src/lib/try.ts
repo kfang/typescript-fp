996a6640db33945e27cc189ae09b5455e4a08121
Kevin Fang <kevin.fang@headspace.com>
2018-10-31 11:39:10 -0700

    use constants to avoid losing `this`

--- src/lib/try.ts
+++ src/lib/try.ts
@@ -1,6 +1,15 @@
 import { Optional } from './optional';
 
 export abstract class Try<A> {
+
+  public static isSuccess<B>(t: Try<B>): boolean {
+    return t.isSuccess;
+  }
+
+  public static isFailure<B>(t: Try<B>): boolean {
+    return t.isFailure;
+  }
+
   public static of<B>(fn: () => B): Try<B> {
     try {
       const value = fn();
@@ -20,62 +29,31 @@ export abstract class Try<A> {
     }
   }
 
+  public abstract readonly isSuccess: boolean;
+  public abstract readonly isFailure: boolean;
+  public abstract readonly get: () => A;
+  public abstract readonly getOrElse: (defaultValue: A) => A;
+  public abstract readonly toOption: () => Optional<A>;
+
   protected readonly value: A;
 
   constructor(v: A) {
     this.value = v;
   }
-
-  public abstract get(): A;
-
-  public abstract getOrElse(defaultValue: A): A;
-
-  public abstract isSuccess(): boolean;
-
-  public abstract isFailure(): boolean;
-
-  public abstract toOption(): Optional<A>;
 }
 
 export class Failure<A> extends Try<A> {
-  public get(): A {
-    throw this.value;
-  }
-  public getOrElse(defaultValue: A): A {
-    return defaultValue;
-  }
-
-  public isSuccess(): boolean {
-    return false;
-  }
-
-  public isFailure(): boolean {
-    return true;
-  }
-
-  public toOption(): Optional<A> {
-    return Optional.of(this.value);
-  }
+  public readonly isSuccess = false;
+  public readonly isFailure = true;
+  public readonly get = () => { throw this.value }
+  public readonly getOrElse = (defaultValue: A) => defaultValue;
+  public readonly toOption: () => Optional<A> = () => Optional.empty();
 }
 
 export class Success<A> extends Try<A> {
-  public get(): A {
-    return this.value;
-  }
-
-  public getOrElse(): A {
-    return this.value;
-  }
-
-  public isSuccess(): boolean {
-    return true;
-  }
-
-  public isFailure(): boolean {
-    return false;
-  }
-
-  public toOption(): Optional<A> {
-    return Optional.empty();
-  }
+  public readonly isSuccess = true;
+  public readonly isFailure = false;
+  public readonly get = () => this.value;
+  public readonly getOrElse = () => this.value;
+  public readonly toOption = () => Optional.of(this.value);
 }
