c88df34d5ef3b047d48a4d40c4c56feda34049fe
Kevin Fang <kevin.fang@headspace.com>
2018-10-31 12:18:40 -0700

    update try impl

--- src/lib/try.ts
+++ src/lib/try.ts
@@ -3,11 +3,11 @@ import { Optional } from './optional';
 export abstract class Try<A> {
 
   public static isSuccess<B>(t: Try<B>): boolean {
-    return t.isSuccess;
+    return t.isSuccess();
   }
 
   public static isFailure<B>(t: Try<B>): boolean {
-    return t.isFailure;
+    return t.isFailure();
   }
 
   public static of<B>(fn: () => B): Try<B> {
@@ -29,31 +29,46 @@ export abstract class Try<A> {
     }
   }
 
-  public abstract readonly isSuccess: boolean;
-  public abstract readonly isFailure: boolean;
+  public abstract readonly isSuccess: () => boolean;
+  public abstract readonly isFailure: () => boolean;
   public abstract readonly get: () => A;
   public abstract readonly getOrElse: (defaultValue: A) => A;
   public abstract readonly toOption: () => Optional<A>;
 
-  protected readonly value: A;
-
-  constructor(v: A) {
-    this.value = v;
-  }
+  public abstract readonly recover: (fn: (e: Error) => A) => Try<A>;
+  public abstract readonly recoverWith: (fn: (e: Error) => Try<A>) => Try<A>;
 }
 
 export class Failure<A> extends Try<A> {
-  public readonly isSuccess = false;
-  public readonly isFailure = true;
-  public readonly get = () => { throw this.value }
+  public readonly error: Error;
+
+  constructor(e: Error) {
+    super();
+    this.error = e;
+  }
+
+  public readonly isSuccess = () => false;
+  public readonly isFailure = () => true;
+  public readonly get = () => { throw this.error}
   public readonly getOrElse = (defaultValue: A) => defaultValue;
   public readonly toOption: () => Optional<A> = () => Optional.empty();
+  public readonly recover = (fn: (e: Error) => A) => new Success(fn(this.error));
+  public readonly recoverWith = (fn: (e: Error) => Try<A>) => fn(this.error);
 }
 
 export class Success<A> extends Try<A> {
-  public readonly isSuccess = true;
-  public readonly isFailure = false;
+  public readonly value: A
+
+  constructor(v: A) {
+    super();
+    this.value = v;
+  }
+
+  public readonly isSuccess = () => true;
+  public readonly isFailure = () => false;
   public readonly get = () => this.value;
   public readonly getOrElse = () => this.value;
   public readonly toOption = () => Optional.of(this.value);
+  public readonly recover = () => this;
+  public readonly recoverWith = () => this;
 }
