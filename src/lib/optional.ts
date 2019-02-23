b170a07ac076280d8949dfb6881b4dbcc746c781
Kevin Fang <kevin.fang@headspace.com>
2019-02-22 17:10:48 -0800

    more docs

--- src/lib/optional.ts
+++ src/lib/optional.ts
@@ -1,6 +1,25 @@
 /**
  * A container object which may or may not contain a non-null value.
  * If a value is present, isEmpty() will return true and get() will return the value.
+ *
+ * Some operations you can do:
+ * ```
+ * const result = Optional.of(1)
+ *   .map(i => i + 1)                   // => Some(2)
+ *   .map(i => i * i)                   // => Some(4)
+ *   .flatMap(i => Optional.of(i - 1))  // => Some(3)
+ *   .pMap(i => Promise.resolve(i));    // => Promise(Some(3))
+ *
+ * const result2 = await result;        // => Some(3)
+ * result2.isEmpty()                    // => false
+ * result2.contains(3)                  // => true
+ * result2.get()                        // => 3
+ *
+ * const result3 = result2              // => Some(3)
+ *   .flatMap(i => Optional.empty())    // => None
+ * result3.get()                        // => throws Error()
+ * result3.getOrElse(4)                 // => 4
+ * ```
  */
 export class Optional<A> {
   public static flatten(o: {
@@ -57,6 +76,12 @@ export class Optional<A> {
    * applies the function to the inner value if it exists.
    * similar to `map` except the function should return an Optional and the final
    * result will be empty if the function returns an empty Optional.
+   *
+   * ```
+   * Optional.of("hello")
+   *   .flatMap(str => Optional.of(str + " world")) // => Some("hello world")
+   *   .flatMap(str => Optional.of(null))           // => None
+   * ```
    * @param {(A) => Optional<B>} fn
    * @returns {Optional<B>}
    */
@@ -66,6 +91,13 @@ export class Optional<A> {
 
   /**
    * returns the inner value
+   *
+   * ```
+   * Optional.of(undefined).get()     // => throws Error
+   * Optional.of(null).get()          // => throws Error
+   * Optional.of("hello world").get() // => "hello world"
+   * ```
+   *
    * @throws {Error} when trying to get a value of an empty Optional
    */
   public get(): A {
@@ -77,6 +109,12 @@ export class Optional<A> {
 
   /**
    * returns the inner value if it exists, otherwise returns the passed in value
+   *
+   * ```
+   * Optional.empty().getOrElse("default")            // => "default"
+   * Optional.of("hello world").getOrElse("default")  // => "hello world"
+   * ```
+   *
    * @param {A} d the default value to return if the Optional is empty
    */
   public getOrElse(d: A): A {
@@ -86,6 +124,11 @@ export class Optional<A> {
   /**
    * returns whether or not the Optional's inner value is equal to what is
    * passed in. Uses `===`
+   *
+   * ```
+   * Optional.of("hello world").contains("hello world")   // => true
+   * Optional.empty().contains("hello world")             // => false
+   * ```
    * @param v
    */
   public contains(v: A): boolean {
@@ -93,7 +136,18 @@ export class Optional<A> {
   }
 
   /**
-   * returns true if this Optional contains `undefined` or `null`, false otherwise
+   * returns true if this Optional contains `undefined` or `null`, false otherwise.
+   *
+   * ```
+   * Optional.of("hello world").isEmpty() // => false
+   * Optional.of(0).isEmpty()             // => false
+   * Optional.of({}).isEmpty()            // => false
+   * Optional.of([]).isEmpty()            // => false
+   * Optional.empty().isEmpty()           // => true
+   * Optional.of(null).isEmpty()          // => true
+   * Optional.of(undefined).isEmpty()     // => true
+   *
+   * ```
    */
   public isEmpty(): boolean {
     return this.v === undefined || this.v === null;
