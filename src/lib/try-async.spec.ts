e39352194101dd49f6c28a527b9b861f9296e10f
Kevin Fang <kfang@users.noreply.github.com>
2020-06-14 07:37:58 -0700

    feat: try-async (#18)
    * add wrapper for working with Promise<Try<A>>
    
    * add tests for the try-async
    
    * adding specs
    
    * add more tests
    
    * add more tests
    
    * linting
    
    * remove package lock
    
    Co-authored-by: Kevin <hkr.fang@gmail.com>

---
+++ src/lib/try-async.spec.ts
@@ -0,0 +1,126 @@
+import test from 'ava';
+import { Try } from './try';
+import { TryAsync } from './try-async';
+
+test('map() runs fn on inner try', async t => {
+  const tryA = Try.success('HELLO');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .map(str => str + ' WORLD!!!')
+    .promise();
+  t.deepEqual(result.get(), 'HELLO WORLD!!!');
+});
+
+test('map() leaves the inner failure', async t => {
+  const error = new Error('Expected Failure');
+  const tryA = Try.failure<string>(error);
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .map(str => str + ' WORLD!!!')
+    .promise();
+  t.truthy(result.isFailure());
+  t.throws(() => result.get(), error.message);
+});
+
+test('map() catches exceptions and converts them to failures', async t => {
+  const error = new Error('Expected Failure');
+  const tryA = Try.success('HELLO');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .map(() => {
+      throw error;
+    })
+    .promise();
+  t.truthy(result.isFailure());
+  t.throws(() => result.get(), error.message);
+});
+
+test('flatMap() returns inner success', async t => {
+  const tryA = Try.success('HELLO');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .flatMap(str => Try.success(str + ' WORLD!!!'))
+    .promise();
+  t.deepEqual(result.get(), 'HELLO WORLD!!!');
+});
+
+test('flatMap() returns inner failure', async t => {
+  const error = new Error('expected failure');
+  const tryA = Try.success('HELLO');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .flatMap(() => Try.failure(error))
+    .promise();
+  t.truthy(result.isFailure());
+  t.throws(() => result.get(), error.message);
+});
+
+test('flatMap() does not run fn on inner failure', async t => {
+  const error = new Error('expected failure');
+  const tryA = Try.failure(error);
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .flatMap(() => Try.success('foobar'))
+    .promise();
+  t.truthy(result.isFailure());
+  t.throws(() => result.get(), error.message);
+});
+
+test('flatMap() returns failure on inner throw', async t => {
+  const error = new Error('expected failure');
+  const tryA = Try.success('foobar');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .flatMap(() => {
+      throw error;
+    })
+    .promise();
+  t.truthy(result.isFailure());
+  t.throws(() => result.get(), error.message);
+});
+
+test('mapAsync() wraps resolved promise', async t => {
+  const tryA = Try.success('hello');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .mapAsync(str => Promise.resolve(str + ' world'))
+    .promise();
+  t.deepEqual(result.get(), 'hello world');
+});
+
+test('mapAsync() wraps failed promise', async t => {
+  const tryA = Try.success('hello');
+  const result = await TryAsync.of(Promise.resolve(tryA))
+    .mapAsync(() => Promise.reject('expected error'))
+    .promise();
+  t.deepEqual(result.isFailure(), true);
+});
+
+test('mapAsync() wraps failed fn', async t => {
+  const tryA = Try.success('Hello');
+  const res = await TryAsync.of(Promise.resolve(tryA))
+    .mapAsync(() => {
+      throw new Error('expected errro');
+    })
+    .promise();
+  t.deepEqual(res.isFailure(), true);
+});
+
+test('flatMapAsync() wraps a failed promise', async t => {
+  const tryA = Try.success(100);
+  const tryF = Try.failure(new Error('expected failure'));
+  const res = await TryAsync.of(Promise.resolve(tryA))
+    .flatMapAsync(() => TryAsync.of(Promise.resolve(tryF)))
+    .promise();
+  t.truthy(res.isFailure());
+});
+
+test('flatMapAsync() maps inner fn', async t => {
+  const tryA = Try.success(100);
+  const res = await TryAsync.of(Promise.resolve(tryA))
+    .flatMapAsync(a => TryAsync.of(Promise.resolve(Try.success(a + 100))))
+    .promise();
+  t.deepEqual(res.get(), 200);
+});
+
+test('flatMapAsync() catches exceptions', async t => {
+  const tryA = Try.success(100);
+  const res = await TryAsync.of(Promise.resolve(tryA))
+    .flatMapAsync(() => {
+      throw new Error('expected error');
+    })
+    .promise();
+  t.deepEqual(res.isFailure(), true);
+});
---
+++ src/lib/try-async.ts
@@ -0,0 +1,51 @@
+import { Try } from './try';
+
+export class TryAsync<A> {
+  public static of<B>(value: Promise<Try<B>>): TryAsync<B> {
+    return new TryAsync<B>(value);
+  }
+
+  private readonly value: Promise<Try<A>>;
+
+  private constructor(value: Promise<Try<A>>) {
+    this.value = value;
+  }
+
+  public map<B>(fn: (value: A) => B): TryAsync<B> {
+    const pTryB = this.value
+      .then(tryA => tryA.map(fn))
+      .catch(error => Try.failure<B>(error));
+    return new TryAsync<B>(pTryB);
+  }
+
+  public flatMap<B>(fn: (value: A) => Try<B>): TryAsync<B> {
+    const pTryB = this.value
+      .then(tryA => tryA.flatMap(fn))
+      .catch(error => Try.failure<B>(error));
+    return new TryAsync<B>(pTryB);
+  }
+
+  public mapAsync<B>(fn: (value: A) => Promise<B>): TryAsync<B> {
+    const pTryB = this.value
+      .then(tryA => tryA.pMap(fn))
+      .catch(error => Try.failure<B>(error));
+    return new TryAsync<B>(pTryB);
+  }
+
+  public flatMapAsync<B>(fn: (value: A) => TryAsync<B>): TryAsync<B> {
+    const pTryB = this.value
+      .then(tryA => {
+        if (tryA.isFailure()) {
+          return Try.failure<B>(tryA.error);
+        } else {
+          return fn(tryA.get()).promise();
+        }
+      })
+      .catch(error => Try.failure<B>(error));
+    return new TryAsync<B>(pTryB);
+  }
+
+  public promise(): Promise<Try<A>> {
+    return this.value;
+  }
+}
