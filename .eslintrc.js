a1a39e59e03896938c197dc60377d63866b25620
Kevin Fang <hkr.fang@gmail.com>
2021-04-02 14:20:07 -0700

    build(eslint): bump eslint version

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,7 @@ module.exports = {
     sourceType: "module" // Allows for the use of imports
   },
   extends: [
-    // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier",
     "plugin:@typescript-eslint/recommended",
-
-    // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "prettier/@typescript-eslint",
-
-    // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-    "plugin:prettier/recommended",
   ]
 };
