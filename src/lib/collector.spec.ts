7e01a62ca8a6ef922acd3f7feef8508eb068e4fd
Kevin Fang <kfang@users.noreply.github.com>
2021-10-25 21:21:28 -0700

    feat: default container for collector; fix interface typing (#49)
    * default Collector.forTry() to empty container
    
    * default Collector.forTryAsync() to empty container
    
    * fix typing for interfaces
    
    Co-authored-by: Kevin Fang <hkr.fang@gmail.com>

--- src/lib/collector.spec.ts
+++ src/lib/collector.spec.ts
@@ -35,6 +35,44 @@ describe("TryAsyncCollector", () => {
         expect(output).toHaveBeenCalledWith({ input: "hello" });
         expect(foobar).not.toHaveBeenCalled();
     });
+
+    it("defaults to an empty container", async () => {
+        const getApple = () => TryAsync.success("apple");
+        const getBanana = () => TryAsync.success("banana");
+        const getPear = () => TryAsync.success("pear");
+
+        const result = await Collector.forTryAsync()
+            .fold("apple", getApple)
+            .fold("banana", getBanana)
+            .fold("pear", getPear)
+            .yield()
+            .promise();
+
+        expect(result.get()).toEqual({
+            apple: "apple",
+            banana: "banana",
+            pear: "pear",
+        });
+    });
+
+    it("allows interfaces to be passed in", async () => {
+        interface FooBar {
+            apple: string;
+            pear: number;
+            banana: boolean;
+        }
+        const foobar: FooBar = { apple: "apple", banana: false, pear: 1234 };
+        const result = await Collector.forTryAsync(foobar)
+            .fold("zig", () => TryAsync.success("zag"))
+            .yield()
+            .promise();
+        expect(result.get()).toEqual({
+            apple: "apple",
+            banana: false,
+            pear: 1234,
+            zig: "zag",
+        });
+    });
 });
 
 describe("TryCollector", () => {
@@ -67,5 +105,41 @@ describe("TryCollector", () => {
         expect(output).toHaveBeenCalledTimes(1);
         expect(output).toHaveBeenCalledWith({ input: "hello" });
         expect(foobar).not.toHaveBeenCalled();
+    });
+
+    it("defaults to an empty container", () => {
+        const getApple = () => Try.success("apple");
+        const getBanana = () => Try.success("banana");
+        const getPear = () => Try.success("pear");
+
+        const result = Collector.forTry()
+            .fold("apple", getApple)
+            .fold("banana", getBanana)
+            .fold("pear", getPear)
+            .yield();
+
+        expect(result.get()).toEqual({
+            apple: "apple",
+            banana: "banana",
+            pear: "pear",
+        });
+    });
+
+    it("allows interfaces to be passed in", () => {
+        interface FooBar {
+            apple: string;
+            pear: number;
+            banana: boolean;
+        }
+        const foobar: FooBar = { apple: "apple", banana: false, pear: 1234 };
+        const result = Collector.forTry(foobar)
+            .fold("zig", () => Try.success("zag"))
+            .yield()
+        expect(result.get()).toEqual({
+            apple: "apple",
+            banana: false,
+            pear: 1234,
+            zig: "zag",
+        });
     });
 });
--- src/lib/collector.ts
+++ src/lib/collector.ts
@@ -2,13 +2,53 @@ import { TryAsync } from "./try-async";
 import { Monad } from "./fantasy";
 import { Try } from "./try";
 
+/**
+ * Collectors allow you to chain and "rollup" monadic containers without having to return
+ * everything passed in at each step. For example, consider you have a sequence of functions
+ * you need to call:
+ *
+ * ```typescript
+ * function getApple(): Try<"apple">;
+ * function getBanana(): Try<"banana">;
+ * function getPear(): Try<"pear">;
+ * ```
+ *
+ * You want the result to be an object that contains the result of all three functions in the
+ * form: `{ apple: "apple", banana: "banana", pear: "pear" }`.
+ *
+ * You could do this by chaining flatMap together but you would have to generate the object as the
+ * return in each of the functions so that the signatures look somthing like this:
+ *
+ * ```typescript
+ * function getApple(): Try<{ apple: "apple" }>;
+ * function getBanana(prev: { apple: "apple" }): Try<{apple: "apple", banana: "banana"}>;
+ * ```
+ *
+ * The alternative is to unwrap each monadic container and wrap everything at the end. This
+ * is not trivial to do and introduces a lot of weird edge cases.  Instead, we can use a
+ * Collector to "collect" these values together:
+ *
+ * ```typescript
+ * const result = Collector.forTry()
+ *   .fold("apple", getApple)
+ *   .fold("banana", getBanana)
+ *   .fold("pear", getPear)
+ *   .yield();
+ *
+ * result.get();
+ * ```
+ */
 export abstract class Collector<O extends Record<string, unknown>> {
 
-    public static forTry<R extends Record<string, unknown>>(init: R): TryCollector<R> {
+    // use of 'any' in typing is a known issue: https://github.com/microsoft/TypeScript/issues/15300
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public static forTry<R extends Record<string, any>>(init: R = {} as R): TryCollector<R> {
         return new TryCollector<R>(Try.of(() => init));
     }
 
-    public static forTryAsync<R extends Record<string, unknown>>(init: R | Promise<R>): TryAsyncCollector<R> {
+    // use of 'any' in typing is a known issue: https://github.com/microsoft/TypeScript/issues/15300
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public static forTryAsync<R extends Record<string, any>>(init: R | Promise<R> = {} as R): TryAsyncCollector<R> {
         return new TryAsyncCollector<R>(TryAsync.of(init));
     }
 
